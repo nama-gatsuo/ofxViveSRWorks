5a59cd16cc44941c7ba35e5d5c45898a2ec2adae
Ayumu Nagamatsu <ayumu.nagamatsu@gmail.com>
2020-03-08 22:48:29 +0900

    [Update] locate missing dlls

--- addon_config.mk
+++ addon_config.mk
@@ -11,4 +11,6 @@ vs:
 	# ADDON_DLLS_TO_COPY += "openvr/bin/win64/openvr_api.dll"
 	ADDON_LIBS		+= "SRWorks_v0.9.0.3_Native_C/libs/ViveSRWorks_Client.lib"
 	ADDON_INCLUDES	+= "SRWorks_v0.9.0.3_Native_C/headers/"
-	ADDON_DLLS_TO_COPY += "$(PROGRAMFILES)/Vive/SRWorks/x64/realsense2.dll"
+	ADDON_DLLS_TO_COPY += "SRWorks_v0.9.0.3_Native_C/x64/Release/ViveSRWorks_Client.dll"
+	ADDON_DLLS_TO_COPY += "SRWorks_v0.9.0.3_Native_C/x64/Release/SRWorks_Log.dll"
+	ADDON_DLLS_TO_COPY += "SRWorks_v0.9.0.3_Native_C/x64/Release/ViveSR_CommunicationRules.dll"
